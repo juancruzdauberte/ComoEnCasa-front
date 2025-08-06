901529dc7f7ab1996b5a4dbc2290de62dcf69879
juancruzdauberte <juandauberte@gmail.com>
2025-08-06 11:04:46 -0300

    modify: Failure page

--- src/components/pages/public/Failure.tsx
+++ src/components/pages/public/Failure.tsx
@@ -1,3 +1,25 @@
 export const Failure = () => {
-  return <div>Failure</div>;
+  return (
+    <section className="flex items-center justify-center min-h-svh">
+      <div className="bg-black/5 shadow-lg rounded-sm px-10 py-16 w-full max-w-4xl flex items-center justify-between gap-14">
+        <div>
+          <picture>
+            <img
+              src="https://res.cloudinary.com/dttpgbmdx/image/upload/v1749124735/LogoComoEnCasaRedondo_gwhyxu.png"
+              alt="logo"
+              loading="lazy"
+              className="h-56"
+            />
+          </picture>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <h1 className="text-4xl text-center font-bold">Accesso Denegado</h1>
+          <p className="text-lg">
+            Contáctate con el administrador para tener mas información.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
