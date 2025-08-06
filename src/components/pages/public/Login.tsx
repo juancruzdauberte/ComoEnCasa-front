a2c377440d367b084f8ba7566e0183d215a8f645
juancruzdauberte <juandauberte@gmail.com>
2025-08-06 10:44:13 -0300

    modify: Login page

--- src/components/pages/public/Login.tsx
+++ src/components/pages/public/Login.tsx
@@ -8,26 +8,30 @@ export const Login = () => {
   };
 
   return (
-    <section className="flex flex-col items-center justify-center min-h-svh">
-      <div>
-        <picture>
-          <img
-            src="https://res.cloudinary.com/dttpgbmdx/image/upload/v1749124735/LogoComoEnCasaRedondo_gwhyxu.png"
-            alt="logo"
-            loading="lazy"
-            className="h-48"
-          />
-        </picture>
-      </div>
+    <section className="flex items-center justify-center min-h-svh">
+      <div className="bg-black/5 shadow-lg rounded-sm px-20 py-16 w-full max-w-4xl flex items-center justify-between gap-8">
+        <div>
+          <picture>
+            <img
+              src="https://res.cloudinary.com/dttpgbmdx/image/upload/v1749124735/LogoComoEnCasaRedondo_gwhyxu.png"
+              alt="logo"
+              loading="lazy"
+              className="h-56"
+            />
+          </picture>
+        </div>
 
-      <div>
-        <h1 className="text-xl text-center">Iniciar Sesión</h1>
-        <button
-          className="bg-red-500 text-white p-2 font-semibold flex items-center gap-2"
-          onClick={handleLogin}
-        >
-          <FcGoogle /> Continuar con google
-        </button>
+        <div className="flex flex-col gap-10">
+          <h1 className="text-5xl text-center font-bold">Iniciar Sesión</h1>
+          <button
+            className="bg-white p-2 font-semibold flex items-center justify-center gap-2 w-full rounded shadow-sm transition duration-200
+             hover:bg-gray-100 "
+            onClick={handleLogin}
+          >
+            <FcGoogle size={30} />
+            <p className="text-xl">Continuar con google</p>
+          </button>
+        </div>
       </div>
     </section>
   );
